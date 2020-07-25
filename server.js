4de4eab2d44223040855e74fa64609f985c2c7ff
Yosr <47481424+YosrSoussia@users.noreply.github.com>
2020-07-25 13:00:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const http = require("http");
+
 
 const requestLondon = axios.get(
   "https://api.openweathermap.org/data/2.5/weather?q=London&appid=c4ee34ee47d5f99aaea1ccf3bc6ffd2d"
@@ -10,9 +10,6 @@ const requestTunisia = axios.get(
 const requestGermany = axios.get(
   "https://api.openweathermap.org/data/2.5/weather?q=Germany&appid=c4ee34ee47d5f99aaea1ccf3bc6ffd2d"
 );
-/*const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');}*/
 
 axios
   .all([requestLondon, requestTunisia, requestGermany])
@@ -24,7 +21,3 @@ axios
     })
   )
   .catch((err) => console.error(err));
-
-/*server.listen(8000, () => {
-  console.log(`Server running at port8000`);
-});*/
